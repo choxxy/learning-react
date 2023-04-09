19bf8e68aaefd404c5a0919fd0327bdd6b6c2f79
Joshua Achoka <jorh.choxxy@gmail.com>
2023-04-09 22:52:56 +0300

    main

--- src/index.js
+++ src/index.js
@@ -1,17 +1,90 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const books = [
+  {
+    id: 1,
+    title: 'Dog Man: Twenty Thousand Fleas Under the Sea: A Graphic Novel',
+    author: 'Dav Pilkey',
+    img: './images/book-1.jpg',
+  },
+  {
+    id: 2,
+    title: 'The Return of the Gods',
+    author: 'Jonathan Cahn',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/913UqrSJf6L._AC_UL127_SR127,127_.jpg',
+  },
+  {
+    id: 3,
+    title: 'The 48 Laws of Power',
+    author: 'Robert Greene',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL127_SR127,127_.jpg',
+  },
+  {
+    id: 4,
+    title: 'Lessons in Chemistry: A Novel',
+    author: 'Bonnie Garmus',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71yNgTMEcpL._AC_UL127_SR127,127_.jpg',
+  },
+]
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const BookList = () => {
+  return (
+    <div>
+      <section className="booklist">
+        {books.map((book) => {
+          return <Book book={book} key={book.id} />
+        })}
+      </section>
+      <section className="booklist">
+        {books.map((book) => {
+          return <BookDestrucuredParams book={book} key={book.id} />
+        })}
+      </section>
+
+      <section className="booklist">
+        {books.map((book) => {
+          return <BookWithSpread {...book} key={book.id} />
+        })}
+      </section>
+    </div>
+  )
+}
+
+
+const BookWithSpread = (props) => {
+  const { img, title, author } = props
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  )
+}
+
+const Book = (props) => {
+  const { img, title, author } = props.book
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  )
+}
+// alternatively
+
+const BookDestrucuredParams = ({ book: { img, title, author } }) => {
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<BookList />)
