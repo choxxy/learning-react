bc187c7e4ca50fd84a1bd4fcb0874f2a3c577032
Joshua Achoka <jorh.choxxy@gmail.com>
2023-04-14 10:25:26 +0300

    Listeners

--- src/index.js
+++ src/index.js
@@ -30,58 +30,122 @@ const books = [
 ]
 
 const BookList = () => {
+  // listeners can alos be passed as props
+  const bookSelectedListener = (bookId) => {
+    const book = books.find((book) => book.id === bookId)
+    alert('Clicked on ' + book.title)
+  }
+
   return (
     <div>
       <section className="booklist">
+        <EventExample />
         {books.map((book) => {
-          return <Book book={book} key={book.id} />
+          return (
+            <Book book={book} key={book.id} selectBook={bookSelectedListener} />
+          )
         })}
       </section>
       <section className="booklist">
         {books.map((book) => {
-          return <BookDestrucuredParams book={book} key={book.id} />
+          return (
+            <BookDestrucuredParams
+              book={book}
+              key={book.id}
+              selectBook={bookSelectedListener}
+            />
+          )
         })}
       </section>
 
       <section className="booklist">
         {books.map((book) => {
-          return <BookWithSpread {...book} key={book.id} />
+          return (
+            <BookWithSpread
+              {...book}
+              key={book.id}
+              selectBook={bookSelectedListener}
+            />
+          )
         })}
       </section>
     </div>
   )
 }
 
+const EventExample = () => {
+  const handleInputChange = (e) => {
+    console.log('handled input change event')
+    console.log(e.target.name)
+    console.log(e.target.value)
+  }
+
+  const handleButtonClick = (e) => {
+    alert('handled button clicked  event')
+  }
+
+  const handleFormSubmission = (e) => {
+    e.preventDefault() // tell js we want to handle submission ourselves
+    alert('handled button clicked  event')
+  }
+  return (
+    <section>
+      <form onSubmit={handleFormSubmission}>
+        <h4> Form Example with input </h4>
+        <input
+          type="text"
+          name="input"
+          onChange={handleInputChange}
+          style={{ margin: '1rem' }}
+        />
+      </form>
+      <button onClick={handleButtonClick}> Click Me!</button>
+    </section>
+  )
+}
 
 const BookWithSpread = (props) => {
-  const { img, title, author } = props
+  const { id, img, title, author } = props
+  const listener = props.selectBook
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      <button onClick={() => listener(id)}>Select Book</button>
     </article>
   )
 }
 
 const Book = (props) => {
-  const { img, title, author } = props.book
+  const { id, img, title, author } = props.book
+  const listener = props.selectBook
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      <button onClick={() => listener(id)}>Select Book</button>
     </article>
   )
 }
 // alternatively
 
-const BookDestrucuredParams = ({ book: { img, title, author } }) => {
+const BookDestrucuredParams = ({
+  book: { id, img, title, author },
+  selectBook,
+}) => {
+
+  // here we use a wrapper
+  const getBook = () => {
+     selectBook(id);
+  }
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      <button onClick={getBook}>Select Book</button>
     </article>
   )
 }
